6e22439a8749e99689f37ee044f4759828ca96f8
Angel Yair <mastercronos990@gmail.com>
2025-10-14 17:16:51 -0600

    Corregida capitalización de la ruta de la imagen en CSS

--- special_messagess.blade.php
+++ special_messagess.blade.php
@@ -24,7 +24,7 @@
       content: "";
       position: fixed;
       top: 0; left: 0; right: 0; bottom: 0;
-      background-image:url('Imagenes/beautiful-heart-tree-landscape-scenery-uhdpaper.com-4K-6.447.jpg');
+      background-image:url('imagenes/beautiful-heart-tree-landscape-scenery-uhdpaper.com-4K-6.447.jpg');
       background-size: cover;
       background-repeat: no-repeat;
       background-position: center;
